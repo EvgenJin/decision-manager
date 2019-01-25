e7e1844834bc5c0d5544243dea07fec4b8f04b05
John Doe <email@email.com>
2019-01-25 16:28:21 +0500

    1
    1

--- index.js
+++ index.js
@@ -45,38 +45,34 @@ db.loadDatabase({},()=> {
       db.saveDatabase();      
     })
     // test request
-    client.on('request',(data)=>{
-      console.log(data)
-      io.sockets.emit('result','resulto!!!')
+    client.on('request',(req)=>{
+      // console.log(data)
+      let res = get_rate(req)
+      io.sockets.emit('result',res)
     })
   })
-// запрос
-let req = {prog:'01.01.00',targ:155,cat_id:0,srok:7,summa:1500000,ask_kds:true,ask_zp:true}
 
-res = base.find().filter((el) =>
-  el.cat_id.includes(req.cat_id)
-  && req.srok >= el.srokmin && req.srok <= el.srokmax
-  && el.prog_list.includes(req.prog)
-  && el.targ_list.includes(req.targ)
-  && req.summa >= el.sum_min && req.summa <= el.summ_max
-);
-
-function get_rate() {
+  // make decision
+function get_rate(req) {
+  res = base.find().filter((el) =>
+    moment(req.date, 'DD.MM.YYYY', true).isBetween(moment(el.dstart, 'DD.MM.YYYY', true), moment(el.dend, 'DD.MM.YYYY', true), 'days', '[]')
+    && el.prog_list.includes(req.prog)
+    && el.targ_list.includes(req.targ)
+    && el.cat_id.includes(req.cat_id)
+    && req.term >= el.srokmin && req.term <= el.srokmax
+    && req.amount >= el.sum_min && req.amount <= el.sum_max
+  );
+  console.log(req)
+  console.log(base.find())
   // беру только первое вхождение
   let obj = res[0]
-  // если решение нашлось
-  if (obj) {
-    if (req.ask_kds) {obj.kds_disc = 0} else {obj.kds_disc = obj.kds_rate}
-    if (req.ask_zp) {obj.zp_disc = 0} else {obj.zp_disc = obj.zp_rate}
+  // если решение не нашлось = 0
+  if (!obj) {
+    return 0
   }
-  return obj.rate - obj.kds_disc - obj.zp_disc  
+  if (req.ask_kds) {obj.kds_disc = 0} else {obj.kds_disc = obj.kds_rate}
+  if (req.ask_zp) {obj.zp_disc = 0} else {obj.zp_disc = obj.zp_rate}
+  return obj.rate + obj.kds_disc + obj.zp_disc
 }
 
 })
-
-/*
-let start = moment('08.11.2018', 'DD.MM.YYYY', true)
-let end = moment('10.11.2018', 'DD.MM.YYYY', true)
-let date = moment('07.11.2018', 'DD.MM.YYYY', true)
-console.log(moment(date).isBetween(start, end, 'days', '[]'))
-*/
